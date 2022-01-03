d1914c8983b0df9bb773abe24f9ef1482e11dfb0
hghddg <81848563+hghddg@users.noreply.github.com>
2022-01-03 15:10:51 +0800

    Authorize View + ID

---
+++ Shared/Domain/BaseDomainModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalManagementv2.Shared.Domain
+{
+  class BaseDomainModel
+  {
+    public int Id { get; set; }
+  }
+}
