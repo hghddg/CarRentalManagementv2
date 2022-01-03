8646bcc3349ab09afc7a926152278f621c0ab668
hghddg <81848563+hghddg@users.noreply.github.com>
2022-01-03 16:45:37 +0800

    f

---
+++ Server/Configuration/Entities/MakeSeedConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using CarRentalManagementv2.Shared.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentalManagementv2.Server.Configuration.Entities
+{
+  public class MakeSeedConfiguration : IEntityTypeConfiguration<Make>
+  {
+    public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Make> builder)
+    {
+      builder.HasData(
+       new Make
+       {
+         Id = 1,
+         Name = "BMW",
+         DateCreated = DateTime.Now,
+         DateUpdated = DateTime.Now,
+         CreatedBy = "System",
+         UpdatedBy = "System"
+       },
+       new Make
+       {
+         Id = 2,
+         Name = "Toyota",
+         DateCreated = DateTime.Now,
+         DateUpdated = DateTime.Now,
+         CreatedBy = "System",
+         UpdatedBy = "System"
+       }
+      );
+
+
+      throw new NotImplementedException();
+    }
+  }
+}
+
