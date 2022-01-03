28a6dcec6bc12705612eade238b1dcb987f498bf
hghddg <81848563+hghddg@users.noreply.github.com>
2022-01-03 14:51:50 +0800

    k

--- Server/Data/Migrations/20220103065129_AddedNameToUser-IgnoreChanges.Designer.cs
+++ Server/Data/Migrations/20220103065129_AddedNameToUser-IgnoreChanges.Designer.cs
@@ -4,14 +4,16 @@ using CarRentalManagementv2.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CarRentalManagementv2.Server.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220103065129_AddedNameToUser-IgnoreChanges")]
+    partial class AddedNameToUserIgnoreChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Server/Data/Migrations/20220103065129_AddedNameToUser-IgnoreChanges.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarRentalManagementv2.Server.Data.Migrations
+{
+    public partial class AddedNameToUserIgnoreChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
